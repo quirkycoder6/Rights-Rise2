4b497a9af39c31f0949ad06515b51c8cc754ccd1
starkz24 <aman.kumarz2404@gmail.com>
2023-09-27 23:59:34 +0530

    path_addedover_unit1

--- client/src/components/units.js
+++ client/src/components/units.js
@@ -1,6 +1,12 @@
 import { GuidebookSvg } from "./svgs";
+import { useNavigate } from "react-router-dom";
 
 const Units = () => {
+
+  const navigate = useNavigate();
+  const handleModuleClick = () => {
+    navigate('/lawquest/module')
+  }
   return (
     <article class="max-w-2xl h-max text-white rounded-2xl bg-yellow-500 border-yellow-600">
       <header class="flex items-center justify-between gap-4 p-4">
@@ -8,7 +14,7 @@ const Units = () => {
           <h2 class="text-2xl font-bold">Unit 1</h2>
           <p class="text-lg">Form basic sentences, greet people</p>
         </div>
-        <h1 class="flex items-center gap-3 rounded-2xl border-2 border-b-4 p-3 transition hover:text-gray-100 border-yellow-600">
+        <h1 onClick={handleModuleClick}  style={{ cursor: "pointer" }} class="flex items-center gap-3 rounded-2xl border-2 border-b-4 p-3 transition hover:text-gray-100 border-yellow-600">
           <GuidebookSvg />
           <span class="sr-only font-bold uppercase lg:not-sr-only">
             Guidebook
