56dacbf3f6090c60f4ec7f578d8abe74c67bc26d
Anand <anotheranand@gmail.com>
2023-09-28 15:52:38 +0530

    fixed routes

--- server/controllers/games.js
+++ server/controllers/games.js
@@ -1,72 +1,72 @@
-const app = express();
-import NumberGuesser from "../models/NumberGuesser";
-import Quiz from "../models/Quiz";
-import Scramble from "../models/Scramble";
-import WordGuesser from "../models/WordGuesser";
+// const app = express();
+// import NumberGuesser from "../models/NumberGuesser";
+// import Quiz from "../models/Quiz";
+// import Scramble from "../models/Scramble";
+// import WordGuesser from "../models/WordGuesser";
 
-app.post('/numberguesser' , async (req, res) => {
-  try {
-    const { userId, question, correct } = req.body;
-    const newGame = new NumberGuesser({
-      userId,
-      question,
-      correct,
-    });
-    await newGame.save();
-    const games = await NumberGuesser.find({ userId });
-    res.status(201).json(games);
-  } catch (error) {
-    res.status(409).json({ message: error.message });
-  }
-});
+// app.post('/numberguesser' , async (req, res) => {
+//   try {
+//     const { userId, question, correct } = req.body;
+//     const newGame = new NumberGuesser({
+//       userId,
+//       question,
+//       correct,
+//     });
+//     await newGame.save();
+//     const games = await NumberGuesser.find({ userId });
+//     res.status(201).json(games);
+//   } catch (error) {
+//     res.status(409).json({ message: error.message });
+//   }
+// });
 
-app.post('/quiz', async (req, res) => {
-  try {
-    const { userId, question, options, correct } = req.body;
-    const newGame = new Quiz({
-      userId,
-      question,
-      options,
-      correct,
-    });
-    await newGame.save();
-    const games = await Quiz.find({ userId });
-    res.status(201).json(games);
-  } catch (error) {
-    res.status(409).json({ message: error.message });
-  }
-})
+// app.post('/quiz', async (req, res) => {
+//   try {
+//     const { userId, question, options, correct } = req.body;
+//     const newGame = new Quiz({
+//       userId,
+//       question,
+//       options,
+//       correct,
+//     });
+//     await newGame.save();
+//     const games = await Quiz.find({ userId });
+//     res.status(201).json(games);
+//   } catch (error) {
+//     res.status(409).json({ message: error.message });
+//   }
+// })
 
-app.post( '/scramble' , async (req, res) => {
-  try {
-    const { userId, question, correct } = req.body;
-    const newGame = new Scramble({
-      userId,
-      question,
-      correct,
-    });
-    await newGame.save();
-    const games = await Scramble.find({ userId });
-    res.status(201).json(games);
-  } catch (error) {
-    res.status(409).json({ message: error.message });
-  }
-})
+// app.post( '/scramble' , async (req, res) => {
+//   try {
+//     const { userId, question, correct } = req.body;
+//     const newGame = new Scramble({
+//       userId,
+//       question,
+//       correct,
+//     });
+//     await newGame.save();
+//     const games = await Scramble.find({ userId });
+//     res.status(201).json(games);
+//   } catch (error) {
+//     res.status(409).json({ message: error.message });
+//   }
+// })
 
-app.post( '/wordguesser',async (req, res) => {
-  try {
-    const { userId, question, correct } = req.body;
-    const newGame = new WordGuesser({
-      userId,
-      question,
-      correct,
-    });
-    await newGame.save();
-    const games = await WordGuesser.find({ userId });
-    res.status(201).json(games);
-  } catch (error) {
-    res.status(409).json({ message: error.message });
-  }
-})
+// app.post( '/wordguesser',async (req, res) => {
+//   try {
+//     const { userId, question, correct } = req.body;
+//     const newGame = new WordGuesser({
+//       userId,
+//       question,
+//       correct,
+//     });
+//     await newGame.save();
+//     const games = await WordGuesser.find({ userId });
+//     res.status(201).json(games);
+//   } catch (error) {
+//     res.status(409).json({ message: error.message });
+//   }
+// })
 
 
--- server/index.js
+++ server/index.js
@@ -3,7 +3,11 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.js";
-
+import NumberGuesser from "./models/Number.js";
+import Quiz from "./models/Quiz.js";
+import Scramble from "./models/Scramble.js";
+import WordGuesser from "./models/WordGuesser.js"; 
+ 
 dotenv.config();
 const app = express();
 app.use(express.json());
@@ -11,6 +15,76 @@ app.use(cors());
 
 app.use("/auth", authRoutes);
  
+app.post("/numberguesser", async (req, res) => {
+  try {
+    const { userId, question, correct } = req.body;
+    const newGame = new NumberGuesser({
+      userId,
+      question,
+      correct,
+    });
+    await newGame.save();
+    const games = await NumberGuesser.find({ userId });
+    res.status(201).json(games);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+});
+
+app.post("/quiz", async (req, res) => {
+  try {
+    const { userId, question, options, correct } = req.body;
+    const newGame = new Quiz({
+      userId,
+      question,
+      options,
+      correct,
+    });
+    await newGame.save();
+    const games = await Quiz.find({ userId });
+    res.status(201).json(games);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+});
+
+app.post("/scramble", async (req, res) => {
+  try {
+    const { userId, question, correct } = req.body;
+    const newGame = new Scramble({
+      userId,
+      question,
+      correct,
+    });
+    await newGame.save();
+    const games = await Scramble.find({ userId });
+    res.status(201).json(games);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+});
+
+app.post("/wordguesser", async (req, res) => {
+  try {
+    const { userId, question, correct } = req.body;
+    const newGame = new WordGuesser({
+      userId,
+      question,
+      correct,
+    });
+    await newGame.save();
+    const games = await WordGuesser.find({ userId });
+    res.status(201).json(games);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+});
+
+
+
+
+
+
 const PORT = process.env.PORT || 6000;
 mongoose
   .connect(process.env.MONGO_URL, {
