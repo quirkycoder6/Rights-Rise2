a3b7b09850dcc41248323ac79dd35c211aa37c88
Anand <anotheranand@gmail.com>
2023-09-28 11:16:03 +0530

    fixed server error

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,7 +1,6 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
-import Questions from '../data/numberguess'
 /* -------------------------------------------------------------------------- */
 /*                                REGISTER USER                               */
 /* -------------------------------------------------------------------------- */
