5795800d1635059ea5141d34318ab7d3114e8842
Anand <anotheranand@gmail.com>
2023-09-29 01:57:01 +0530

    added genAi in number guesser

--- client/src/numberguess/NumberGuessMaker.js
+++ client/src/numberguess/NumberGuessMaker.js
@@ -1,13 +1,71 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./makerstyle.css";
+import OpenAI from "openai";
 import { useSelector } from "react-redux";
+import QuestionAnswerList from "./displayGen";
+
+function extractQuestionsAndAnswers(text) {
+  const questionRegex = /Question:\s(.+?)\sAnswer:\s(.+?)(?=\n\n|\n$|$)/gs;
+  const matches = [...text.matchAll(questionRegex)];
+
+  if (!matches) {
+    return { questions: [], answers: [] };
+  }
+
+  const questions = matches.map((match) => match[1].trim());
+  const answers = matches.map((match) => {
+    const answer = match[2].trim();
+    const numericPart = answer.match(/\d+/); // Extract numeric part
+    return numericPart ? parseInt(numericPart[0]) : null; // Convert to integer
+  });
+
+  return { questions, answers }; 
+}
 
 const NumberGuesserForm = () => {
   // const [userId, setUserId] = useState('');
   const [question, setQuestion] = useState("");
   const [correct, setCorrect] = useState("");
+  const [genQuestion, setGenQuestion] = useState([]);
+  const [genAns, setGenAns] = useState([]);
   const [message, setMessage] = useState("");
   const userId = useSelector((state) => state.user._id);
+  const msg = `
+ Create 10 number guesser questions about right to education. Your response should strictly follow the below mentioned structure. Your question should test education related articles knowledge written in the Indian law . It shouldn't be too simple.
+  You shouldn't ask the same question everytime. Don't mention question numbers.
+  "
+  Question: The article number related question
+  Answer: Article Number (Dont include "Article" Word)
+  "
+  `;
+
+  const openai = new OpenAI({
+    apiKey: "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",
+    dangerouslyAllowBrowser: true,
+  });
+
+  async function main() {
+    try {
+      const chatCompletion = await openai.chat.completions.create({
+        messages: [{ role: "user", content: msg }],
+        model: "gpt-3.5-turbo",
+      });
+
+      console.log(chatCompletion.choices[0].message.content);
+      const { questions, answers } = extractQuestionsAndAnswers(
+        chatCompletion.choices[0].message.content
+      );
+      setGenQuestion(questions);
+      setGenAns(answers);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+useEffect(() => {
+  console.log(genQuestion);
+  console.log(genAns);
+}, [genQuestion, genAns]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -38,6 +96,12 @@ const NumberGuesserForm = () => {
   return (
     <div className="container">
       <h2>Create a Number Guesser Game</h2>
+      <button onClick={main}>Generate</button>
+      {genQuestion.length > 0 && genAns.length ? (
+        <QuestionAnswerList genQuestion={genQuestion} genAns={genAns} />
+      ) : (
+        <></>
+      )}
       <form onSubmit={handleSubmit}>
         {/* <div>
           <label htmlFor="userId">User ID:</label>
