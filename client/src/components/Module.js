a8773a4ad1fdfd6b37e7cfaa069d5009158e19fd
starkz24 <aman.kumarz2404@gmail.com>
2023-09-28 23:16:43 +0530

    game_page_modified

--- client/src/components/Module.js
+++ client/src/components/Module.js
@@ -10,24 +10,35 @@ const Module = () => {
   const isAuth = Boolean(useSelector((state) => state.token));
 
   return (
-    <div className="App">
-      <h1>Navigate to quiz</h1>
-      {isAuth && <Link to="/lawquest/module/quiz" className="btn btn-primary">Quiz</Link>}
-
-      <h1>Navigate to Number guess</h1>
-      {isAuth && <Link to="/lawquest/module/numberguess" className="btn btn-primary">Number Guess</Link>}
-
-      <h1>Navigate to Number guess Maker</h1>
-      {isAuth && <Link to="/lawquest/module/numberguessmaker" className="btn btn-primary">Number Guess Maker</Link>}
-
-      <h1>Navigate to Word Guess</h1>
-      {isAuth && <Link to="/lawquest/module/wordguess" className="btn btn-primary">Word Guess</Link>}
-
-      <h1>Navigate to Word Scramble Maker</h1>
-      {isAuth && <Link to="/lawquest/module/wordscramblemaker" className="btn btn-primary">Word Scramble Maker</Link>}
-
-      <h1>Navigate to Jigsaw Puzzle</h1>
-      {isAuth && <Link to="/lawquest/module/jigsaw" className="btn btn-primary">Jigsaw Puzzle</Link>}
+    <div className="flex-col flex">
+      <div className="my-5">
+        <Navbar />
+      </div>
+      <div className="flex justify-around gap-3 mt-7 px-5">
+        <div className="hidden md:block">
+          <Leftbar />
+        </div>
+        <div className="flex justify-center lg:justify-around sm:gap-3 flex-grow">
+          <div className="flex items-center flex-col gap-4">
+            {isAuth && <Link to="/lawquest/module/quiz" className="btn btn-primary">Quiz Game</Link>}
+            {isAuth && <Link to="/lawquest/module/numberguess" className="btn btn-primary">Number Guesser</Link>}
+            {isAuth && <Link to="/lawquest/module/numberguessmaker" className="btn btn-primary">Number Guess Maker</Link>}
+            {isAuth && <Link to="/lawquest/module/wordguess" className="btn btn-primary">Word Guesser</Link>}
+            {isAuth && <Link to="/lawquest/module/jigsaw" className="btn btn-primary">Jigsaw Puzzle</Link>}
+            {isAuth && <Link to="/lawquest/module/wordscramblemaker" className="btn btn-primary">Word Scramble Maker</Link>}
+          </div>
+          <div className="flex items-center flex-col gap-5">
+            <Dailyquest />
+            <Dailyquest />
+            <Dailyquest />
+          </div>
+        </div>
+      </div>
+      <div className="fixed bottom-0 w-full mb-3 md:hidden bg-white border-gray-300">
+        <div className="flex justify-around items-center">
+          <Leftbar />
+        </div>
+      </div>
     </div>
   );
 }
