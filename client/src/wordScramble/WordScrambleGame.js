3aad67517690c905ee863b1a873cbf0c8d661745
starkz24 <aman.kumarz2404@gmail.com>
2023-09-29 13:17:14 +0530

    timer

--- client/src/wordScramble/WordScrambleGame.js
+++ client/src/wordScramble/WordScrambleGame.js
@@ -1,29 +1,31 @@
 import React, { useState, useEffect } from 'react';
-import { words } from './data';
 import './WordScrambleGame.css';
+import {words} from './data'
 
-const WordScrambleGame = () => {
+function App() {
+  const [word, setWord] = useState('');
+  const [hint, setHint] = useState('');
+  const [timeLeft, setTimeLeft] = useState(30);
+  const [userInput, setUserInput] = useState('');
   const [correctWord, setCorrectWord] = useState('');
-  const [maxTime, setMaxTime] = useState(30);
-  const [timer, setTimer] = useState(null);
-  const [userWord, setUserWord] = useState('');
+  const [isGameRunning, setIsGameRunning] = useState(false);
 
-  useEffect(() => {
-    initGame();
-  }, []);
+  let timer;
 
   const initTimer = () => {
     clearInterval(timer);
-    const newTimer = setInterval(() => {
-      if (maxTime > 0) {
-        setMaxTime(maxTime - 1);
-      } else {
-        clearInterval(newTimer);
-        alert(`Time off! ${correctWord.toUpperCase()} was the correct word`);
-        initGame();
-      }
+    timer = setInterval(() => {
+      setTimeLeft((prevTimeLeft) => {
+        if (prevTimeLeft > 0) {
+          return prevTimeLeft - 1;
+        } else {
+          clearInterval(timer);
+          alert(`Time off! ${correctWord.toUpperCase()} was the correct word`);
+          initGame(); // Reset the game after the time is up
+          return 0;
+        }
+      });
     }, 1000);
-    setTimer(newTimer);
   };
 
   const initGame = () => {
@@ -34,52 +36,57 @@ const WordScrambleGame = () => {
       const j = Math.floor(Math.random() * (i + 1));
       [wordArray[i], wordArray[j]] = [wordArray[j], wordArray[i]];
     }
+    setWord(wordArray.join(''));
+    setHint(randomObj.hint);
     setCorrectWord(randomObj.word.toLowerCase());
-    setUserWord('');
-    wordText.innerText = wordArray.join('');
-    hintText.innerText = randomObj.hint;
-    inputField.value = '';
-    inputField.setAttribute('maxlength', correctWord.length);
+    setUserInput('');
+    setIsGameRunning(true);
   };
 
+  useEffect(() => {
+    initGame();
+  }, []);
+
   const checkWord = () => {
-    const userWordLowerCase = userWord.toLowerCase();
-    if (!userWordLowerCase) return alert('Please enter the word to check!');
-    if (userWordLowerCase !== correctWord) return alert(`Oops! ${userWordLowerCase} is not the correct word`);
+    const userWord = userInput.toLowerCase();
+    if (!userWord) return alert('Please enter the word to check!');
+    if (userWord !== correctWord) return alert(`Oops! ${userWord} is not a correct word`);
+    clearInterval(timer); // Stop the timer when the word is correct
     alert(`Congrats! ${correctWord.toUpperCase()} is the correct word`);
     initGame();
   };
 
-  let wordText, hintText, inputField;
+  const resetGame = () => {
+    clearInterval(timer);
+    setTimeLeft(30);
+    setIsGameRunning(false);
+    setUserInput('');
+  };
 
   return (
     <div className="container">
       <h2>Word Scramble</h2>
       <div className="content">
-        <p ref={(element) => (wordText = element)} className="word"></p>
+        <p className="word">{word}</p>
         <div className="details">
-          <p className="hint">
-            Hint: <span ref={(element) => (hintText = element)}></span>
-          </p>
-          <p className="time">
-            Time Left: <span><b>{maxTime}</b>s</span>
-          </p>
+          <p className="hint">Hint: <span>{hint}</span></p>
+          <p className="time">Time Left: <span><b>{timeLeft}</b>s</span></p>
         </div>
         <input
-          ref={(input) => (inputField = input)}
           type="text"
           spellCheck="false"
           placeholder="Enter a valid word"
-          value={userWord}
-          onChange={(e) => setUserWord(e.target.value)}
+          value={userInput}
+          onChange={(e) => setUserInput(e.target.value)}
+          disabled={!isGameRunning}
         />
         <div className="buttons">
-          <button className="refresh-word" onClick={initGame}>Refresh Word</button>
-          <button className="check-word" onClick={checkWord}>Check Word</button>
+          <button className="refresh-word" onClick={resetGame}>Refresh Word</button>
+          <button className="check-word" onClick={checkWord} disabled={!isGameRunning}>Check Word</button>
         </div>
       </div>
     </div>
   );
-};
+}
 
-export default WordScrambleGame;
+export default App;
